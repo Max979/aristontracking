8f09ea4ddc6c1fd5639186bf341c0e8015a9c6e3
Max979 <45206947+Max979@users.noreply.github.com>
2018-11-20 16:30:59 +0100

    Create globalsnippet.js

---
+++ globalsnippet.js
@@ -0,0 +1,14 @@
+<!-- 
+Start of global snippet: Please do not remove
+Place this snippet between the <head> and </head> tags on every page of your site.
+-->
+<!-- Global site tag (gtag.js) - Google Marketing Platform -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=DC-4684015"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'DC-4684015');
+</script>
+<!-- End of global snippet: Please do not remove -->
