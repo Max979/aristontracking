f006652150034da929c94280f9109899da79188b
Max979 <45206947+Max979@users.noreply.github.com>
2018-11-20 16:37:19 +0100

    Update globalsnippet.js

--- globalsnippet.js
+++ globalsnippet.js
@@ -13,17 +13,17 @@ Place this snippet between the <head> and </head> tags on every page of your sit
 </script>
 <!-- End of global snippet: Please do not remove -->
 <!--
-Event snippet for HP_Ariston_Novembre18 on https://www.ariston.com/ita/it/: Please do not remove.
+Event snippet for LP_Ariston_Alteas_One_Net_Novembre18 on https://www.ariston.com/ita/it/prodotti/caldaie/caldaie-condensazione/alteas-one-net: Please do not remove.
 Place this snippet on pages with events that you’re tracking. 
 Creation date: 11/07/2018
 -->
 <script>
   gtag('event', 'conversion', {
     'allow_custom_scripts': true,
-    'send_to': 'DC-4684015/lpxwv0/hp_ar0+unique'
+    'send_to': 'DC-4684015/lpxwv0/lp_ar002+unique'
   });
 </script>
 <noscript>
-<img src="https://ad.doubleclick.net/ddm/activity/src=4684015;type=lpxwv0;cat=hp_ar0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;ord=1;num=1?" width="1" height="1" alt=""/>
+<img src="https://ad.doubleclick.net/ddm/activity/src=4684015;type=lpxwv0;cat=lp_ar002;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;ord=1;num=1?" width="1" height="1" alt=""/>
 </noscript>
 <!-- End of event snippet: Please do not remove -->
